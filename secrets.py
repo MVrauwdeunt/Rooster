34f78628b6ddfd0949d116d531874d541d824f24
Michel <zanbee@bragi.fritz.box>
2022-05-30 14:48:05 +0200

    Add absolut path

--- secrets.py
+++ secrets.py
@@ -2,9 +2,12 @@
 # coding: utf8
 
 import yaml
+import os
+
 
 def read_secrets():
-    secrets_path = "config.yaml"
+    secrets_path = os.path.dirname(os.path.abspath(__file__)) + "/config.yaml"
     with open(secrets_path, "r") as f:
         secrets = yaml.safe_load(f)
         return secrets
+
