daff6d7a4dcac97a22d2d3006d3b26e2421bc0f9
Michel <zanbee@bragi.fritz.box>
2022-09-29 09:50:39 +0200

    test

--- ics.py
+++ ics.py
@@ -75,8 +75,8 @@ def create_event(rooster):
         print(ics)
         # my_event = my_work_calendar.save_event(ics)
 
-schedule.every().hour.do(create_event(rooster))
-while True:
-    schedule.run_pending()
-    # create_event(rooster)
+#schedule.every().hour.do(create_event(rooster))
+#while True:
+#    schedule.run_pending()
+create_event(rooster)
     # time.sleep(3600)  # do work every one hour
